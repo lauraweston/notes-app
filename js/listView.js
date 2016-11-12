a7fd629991727b96e76490cb46a16119f49089f9
Laura Weston <laura.weston@outlook.com>
2016-11-12 21:52:18 +0000

    Refactor listView in to module pattern

--- js/listView.js
+++ js/listView.js
@@ -1,23 +1,28 @@
-var ListView = function (listModel) {
-    this.listModel = listModel;
-};
+(function (exports) {
 
-ListView.prototype.displayList = function () {
-    var items = this.createListItems();
-    this.appendItemsToList(items);
-};
+    var createListItems = function (listModel) {
+        return listModel.allNotes().map(function (note) {
+            var item = document.createElement("li");
+            item.innerHTML = note.truncateText();
+            return item;
+        });
+    };
 
-ListView.prototype.createListItems = function () {
-    return this.listModel.allNotes().map(function (note) {
-        var item = document.createElement("li");
-        item.innerHTML = note.truncateText();
-        return item;
-    });
-};
+    var appendItemsToList = function (items) {
+        var list = document.getElementById("list");
+        items.forEach(function (item) {
+            list.appendChild(item);
+        });
+    };
 
-ListView.prototype.appendItemsToList = function (items) {
-    var list = document.getElementById("list");
-    items.forEach(function (item) {
-        list.appendChild(item);
-    });
-};
+    var ListView = function (listModel) {
+        this.listModel = listModel;
+    };
+
+    ListView.prototype.displayList = function () {
+        var items = createListItems(this.listModel);
+        appendItemsToList(items);
+    };
+
+    exports.ListView = ListView;
+})(this);
