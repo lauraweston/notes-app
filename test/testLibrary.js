51d78219188cc089317ffa09a6a8a7b89b9e71ba
Laura Weston <laura.weston@outlook.com>
2016-11-12 14:34:19 +0000

    Create new expect test matcher: toBeUndefined

--- test/testLibrary.js
+++ test/testLibrary.js
@@ -22,6 +22,11 @@ var expect = function (actualValue) {
             if (!actualValue.includes(expectedValue)) {
                 throw new Error("Expected " + actualValue + " to include " + expectedValue);
             }
+        },
+        toBeUndefined: function () {
+            if (actualValue !== undefined) {
+                throw new Error("Expected " + actualValue + " to be undefined");
+            }
         }
     };
 };
