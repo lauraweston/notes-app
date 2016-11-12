ac724c8d4f727d33e318b9d141a0bae1476ff0b2
Laura Weston <laura.weston@outlook.com>
2016-11-12 11:19:47 +0000

    Apply module pattern to noteModel

--- js/noteModel.js
+++ js/noteModel.js
@@ -1,12 +1,17 @@
-var Note = function (text) {
-    this.text = text;
-}
+(function (exports) {
+    var Note = function (text) {
+        this.text = text;
+    }
 
-Note.prototype.fullText = function () {
-    return this.text;
-}
+    Note.prototype.fullText = function () {
+        return this.text;
+    }
 
-Note.prototype.truncateText = function () {
-    return this.text.length > 20 ? 
-        this.text.slice(0, 20) + "&hellip;" : this.text;
-}
+    Note.prototype.truncateText = function () {
+        return this.text.length > 20 ?
+            this.text.slice(0, 20) + "&hellip;" : this.text;
+    }
+    
+    exports.Note = Note;
+})(this);
+
