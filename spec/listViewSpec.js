be10b350bb7b8d99e12332b09cbf81c1dbf83b52
Laura Weston <laura.weston@outlook.com>
2016-11-12 21:44:59 +0000

    Refactor listViewSpec using beforeEach

--- spec/listViewSpec.js
+++ spec/listViewSpec.js
@@ -1,44 +1,38 @@
 (function () {
     window.addEventListener("load", function () {
-        var resetList = function () {
-            var listElement = document.getElementById("list");
+        var listModel;
+        var listElement;
+
+        beforeEach(function () {
+            listElement = document.getElementById("list");
             listElement.innerHTML = "";
-        };
+            listModel = new ListModel();
+        });
 
         it("should display a given list with one note in the browser", function () {
-            resetList();
-
-            var listModel = new ListModel();
             listModel.newNote("My first note");
             var listView = new ListView(listModel);
             listView.displayList();
-            var listElement = document.getElementById("list");
 
             expect(listElement.innerHTML).toEqual("<li>My first note</li>");
         });
-        it("should display a given list with multiple notes in the browser", function () {
-            resetList();
 
-            var listModel = new ListModel();
+        it("should display a given list with multiple notes in the browser", function () {
             listModel.newNote("My first note");
             listModel.newNote("My second note");
             listModel.newNote("My third note");
             var listView = new ListView(listModel);
             listView.displayList();
-            var listElement = document.getElementById("list");
 
             expect(listElement.childElementCount).toEqual(3);
             expect(listElement.children[0].outerHTML).toEqual("<li>My first note</li>");
             expect(listElement.children[1].outerHTML).toEqual("<li>My second note</li>");
             expect(listElement.children[2].outerHTML).toEqual("<li>My third note</li>");
         });
+        
         it("should not display a given empty list in the browser", function () {
-            resetList();
-
-            var listModel = new ListModel();
             var listView = new ListView(listModel);
             listView.displayList();
-            var listElement = document.getElementById("list");
 
             expect(listElement.childElementCount).toEqual(0);
         });
