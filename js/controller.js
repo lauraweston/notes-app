ba3426cbc5e7f3a697a09d95dd92859680dce374
Laura Weston <laura.weston@outlook.com>
2016-11-13 14:31:35 +0000

    Controller creates new notes

--- js/controller.js
+++ js/controller.js
@@ -17,7 +17,11 @@
     Controller.prototype.renderView = function(View, model) {
         this.renderedView = new View(model);
         this.renderedView.render();
-    }
+    };
+
+    Controller.prototype.createNote = function (text) {
+        this.listModel.newNote(text);
+    };
 
     exports.Controller = Controller;
 })(this);
--- spec/controllerSpec.js
+++ spec/controllerSpec.js
@@ -31,5 +31,12 @@
             expect(controller.renderedView.constructor).toEqual(NoteViewDouble);
             expect(controller.renderedView.noteModel).toEqual(listModel.notes[0]);
         });
+        it("adds new note to the list model from the Controller", function () {
+            controller.createNote("My second note");
+
+            expect(controller.listModel.notes.length).toEqual(2);
+            expect(controller.listModel.notes[1].constructor).toEqual(NoteModel);
+            expect(controller.listModel.notes[1].fullText()).toEqual("My second note");
+        });
     });
 })();
