1a2dc6c7df99f874e6b6edb5b8bf8f477384d1c5
Laura Weston <laura.weston@outlook.com>
2016-11-13 11:27:17 +0000

    Add beforeEach to noteView to reset html before each test; add test to check #note is empty on noteView initialisation

--- spec/noteViewSpec.js
+++ spec/noteViewSpec.js
@@ -1,14 +1,26 @@
+
+
 (function () {
     window.addEventListener("load", function () {
-        it("shows a note on a single page", function () {
-            var noteModel = new NoteModel("My first note");
+
+        var noteElement;
+
+        beforeEach(function () {
+            noteElement = document.getElementById("note");
+            noteElement.innerHTML = "";
+        });
+
+        it("shows full text of a single note on a single page", function () {
+            var noteModel = new NoteModel("Stuff to do: thing 1, thing 2, thing 3, thing 4");
             var noteView = new NoteView(noteModel);
 
             noteView.displayNote();
 
-            var noteElement = document.getElementById("note");
-            expect(noteElement.innerHTML).toEqual("<p>My first note</p>");
+            expect(noteElement.children.length).toEqual(1);
+            expect(noteElement.innerHTML).toEqual("<p>Stuff to do: thing 1, thing 2, thing 3, thing 4</p>");
         });
-
+        it("should have an empty #note element on initialising noteView", function () {
+            expect(noteElement.children.length).toEqual(0);
+        });
     });
 })();
